7d00a43daff68a9cbe1c9ea847e7f1b0f8d89d18
Muaviyaimran <muaviyaimran1122@gmail.com>
2025-03-24 11:17:47 +0500

    Completed Section 2

--- day_one/profitCalaculator/profit_calculator.go
+++ day_one/profitCalaculator/profit_calculator.go
@@ -1,28 +1,69 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 )
 
+// Goals
+// 1) Validate user input
+//    => Show error message & exit if invalid input is provided
+//    - No negative numbers
+//    - Not 0
+// 2) Store calculated results into file
+
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	revenue, err := getUserInput("Revenue: ")
 
-	fmt.Print("Enter revenue: ")
-	fmt.Scan(&revenue)
+	if err != nil {
+		fmt.Println(err)
+		return
+		// panic(err)
+	}
 
-	fmt.Print("Enter expenses: ")
-	fmt.Scan(&expenses)
+	expenses, err := getUserInput("Expenses: ")
 
-	fmt.Print("Enter tax rate: ")
-	fmt.Scan(&taxRate)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
+	taxRate, err := getUserInput("Tax Rate: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
+	storeResults(ebt, profit, ratio)
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
 	ebt := revenue - expenses
 	profit := ebt * (1 - taxRate/100)
 	ratio := ebt / profit
+	return ebt, profit, ratio
+}
 
-	fmt.Printf("EBT value is: %v\n", ebt)
-	fmt.Printf("Future real value is %.2f\n", profit)
-	fmt.Printf("Ratio is: %.2f\n", ratio)
+func getUserInput(infoText string) (float64, error) {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+
+	if userInput <= 0 {
+		return 0, errors.New("value must be a positive number")
+	}
+
+	return userInput, nil
 }
